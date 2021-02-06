dc5751c209ac4d90055003442ceb0b2e7c8e073a
advseguridad <66281269+advseguridad@users.noreply.github.com>
2021-02-06 11:46:30 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Layout, Modal, Menu, Form, Input, Button} from 'antd';
 import 'antd/dist/antd.css';
 import { Component, useState } from 'react'
 
-//AG GRID 
+//AG GRID
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
@@ -12,30 +12,34 @@ const {Header, Content, Footer } = Layout;
 
 class App extends Component {
 
-  state = { 
-    visible : false, 
-    personalizado : false, 
-    colonoVehicular : 0, 
-    colonoPeatonal : 0,
-    personalPeatonal : 0,
-    personalVehicular : 0,
-    visitasVip : 0
-  };
+    //Hay que iniciar el estado en el constructor de la clase
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible : false,
+            personalizado : false,
+            colonoVehicular : 0,
+            colonoPeatonal : 0,
+            personalPeatonal : 0,
+            personalVehicular : 0,
+            visitasVip : 0
+        };
+    }
 
   _handleClick = (e) => {
-      this.setState( { visible : e.key } );
+      this.setState( { visible : true } );
   }
 
   _handleClickConf = (e) => {
     this.setState( { personalizado : e.key } );
   }
 
-  _setVisible = () => 
+  _setVisible = () =>
   {
     this.setState( { visible : false } );
   }
 
-  _handleSave = () => 
+  _handleSave = () =>
   {
     alert(`DATOS PARA GUARDAR \n${JSON.stringify(this.state)}`);
   }
@@ -51,7 +55,12 @@ class App extends Component {
   }
 
   render()
-  {
+  {   //Donde asignas las columnas, el campo field debe coincidir con las propiedades del arreglo de objetos que llega
+      const rowData = [
+          {Id: 1, Nombre: "Test", Direccion: "Test direccion", FechaAlta: "2021-12-12", Registrador: "yo", Tipo: "peatonal", Accesos: "asdf", Facial: "asdf"},
+          {Id: 2, Nombre: "Test 2", Direccion: "Test direccion 2", FechaAlta: "2021-12-12", Registrador: "yo", Tipo: "peatonal", Accesos: "asdf", Facial: "asdf"},
+          {Id: 3, Nombre: "Test 3", Direccion: "Test direccion 3", FechaAlta: "2021-12-12", Registrador: "yo", Tipo: "peatonal", Accesos: "asdf", Facial: "asdf"},
+      ];
     return (
       <Layout className='App'>
         <Header style={{ position: 'fixed', zIndex: 1, width: '90%' }}>
@@ -65,24 +74,27 @@ class App extends Component {
           <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
             <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
               <AgGridReact
-                  // rowData={rowData}
+                  rowData={rowData}
                   >
-                  <AgGridColumn field="Id"></AgGridColumn>
-                  <AgGridColumn field="Nombre"></AgGridColumn>
-                  <AgGridColumn field="Direccion"></AgGridColumn>
-                  <AgGridColumn field="Fecha alta"></AgGridColumn>
-                  <AgGridColumn field="Registrador"></AgGridColumn>
-                  <AgGridColumn field="Tipo"></AgGridColumn>
-                  <AgGridColumn field="Accesos"></AgGridColumn>
-                  <AgGridColumn field="Facial"></AgGridColumn>
+                  <AgGridColumn field="Id" />
+                  <AgGridColumn field="Nombre" />
+                  <AgGridColumn field="Direccion" />
+                  <AgGridColumn field="FechaAlta" />
+                  <AgGridColumn field="Registrador" />
+                  <AgGridColumn field="Tipo" />
+                  <AgGridColumn field="Accesos" />
+                  <AgGridColumn field="Facial" />
               </AgGridReact>
-            </div>  
+            </div>
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>ALL RIGHTS RESERVED</Footer>
-
-        {this.state.visible == "true"
-        ? 
+          {/*Como buena práctica utilizar === para comparar, al no ser tipado el lenguaje puedes hacer esta
+          comparación aun que visible es booleana y "true" está como string retorna verdadero.
+          Al ponerla así: visible === "true" ahora si retornaría falso porque con === también valida que
+          las 2 variables son del mismo tipo*/}
+        {this.state.visible === true
+        ?
           <Modal
               title="PANTALLA DE CONFIGURACIÓN"
               centered
@@ -100,17 +112,17 @@ class App extends Component {
                 </Header>
                 <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
                 <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-                    {this.state.personalizado == "true" 
-                    ? 
+                    {this.state.personalizado == "true"
+                    ?
                       <div>
-                          THIS IS ANOTHER TABLE OF AG-GRID                          
+                          THIS IS ANOTHER TABLE OF AG-GRID
                       </div>
-                    : 
-                    
+                    :
+
                     <Form method="POST" onSubmitCapture={this._handleSave}>
                         <Form.Item label="COLÓNO VEHICULAR">
                             <Input placeholder="0" id="colonoVehicular" name="colonoVehicular" style={{width:'70%'}} type="number" min="0" required={true} onChange={this._handleChange} />
-                        </Form.Item> 
+                        </Form.Item>
                         <Form.Item label="COLÓNO PEATONAL">
                             <Input placeholder="0" id="colonoPeatonal" name="colonoPeatonal" style={{width:'70%'}} onChange={this._handleChange} type="number" min="0" required={true} />
                         </Form.Item>
@@ -126,11 +138,11 @@ class App extends Component {
                         <Form.Item >
                             <Button onClick={this._handleSave} type="primary">Guardar</Button>
                         </Form.Item>
-                    </Form>  
-                } 
+                    </Form>
+                }
                 </div>
                 </Content>
-            </Layout>  
+            </Layout>
           </Modal>
        : this._setVisible
       }
